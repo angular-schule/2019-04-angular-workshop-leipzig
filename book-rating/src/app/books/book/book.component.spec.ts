7eaebd9428a43824a4da37530c0accb65370bc35
Ferdinand Malcher <mail@ferdinand-malcher.de>
2019-04-25 14:31:32 +0200

    Test BookComponent

--- book-rating/src/app/books/book/book.component.spec.ts
+++ book-rating/src/app/books/book/book.component.spec.ts
@@ -1,14 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BookComponent } from './book.component';
+import { Book } from '../shared/book';
+import { ButtonSharedModule } from 'src/app/button-shared/button-shared.module';
+import { By } from '@angular/platform-browser';
 
-describe('BookComponent', () => {
+fdescribe('BookComponent', () => {
   let component: BookComponent;
   let fixture: ComponentFixture<BookComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BookComponent ]
+      declarations: [ BookComponent ],
+      imports: [ButtonSharedModule] // Achtung: Später vermeiden mit Shallow Unit Test
     })
     .compileComponents();
   }));
@@ -16,10 +20,52 @@ describe('BookComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
+
+    // Initialisierung VOR detectChanges()
+    component.book = {
+      isbn: '',
+      title: 'title',
+      description: 'ddd',
+      price: 2.99,
+      rating: 3
+    };
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit event on doRateUp', () => {
+    // Arrange
+    let eventBook: Book;
+
+    component.rateUp.subscribe(b => {
+      eventBook = b;
+    });
+
+    // Act
+    component.doRateUp();
+
+    // Assert
+    expect(eventBook).toBeTruthy();
+    expect(eventBook).toBe(component.book);
+  });
+
+  it('should call method for button click', () => {
+    // Spionage!
+    spyOn(component, 'doRateUp');
+
+    // Button holen
+    const rateUpBtn = fixture.debugElement
+      .query(By.css('[data-testing-id="rateUpBtn"]'))
+      .nativeElement;
+
+    // klicken
+    rateUpBtn.click();
+
+    // prüfen, ob Methode aufgerufen
+    expect(component.doRateUp).toHaveBeenCalledTimes(1);
   });
 });
