2c9fbc15c6fb2fa18b22e9016b310660e28e5b4a
Ferdinand Malcher <mail@ferdinand-malcher.de>
2019-04-25 12:03:26 +0200

    Bücher bewerten und sortieren

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
 
 @Component({
   selector: 'br-dashboard',
@@ -12,7 +13,7 @@ export class DashboardComponent implements OnInit {
   myUrl = '//angular.schule';
   myClasses = 'text-success';
 
-  constructor() { }
+  constructor(private rs: BookRatingService) { }
 
   ngOnInit() {
     this.books = [
@@ -31,6 +32,22 @@ export class DashboardComponent implements OnInit {
         price: 32.9
       }
     ];
+  }
+
+  rateUp(book: Book) {
+    const ratedBook = this.rs.rateUp(book);
+    this.updateSortList(ratedBook);
+  }
+
+  rateDown(book: Book) {
+    const ratedBook = this.rs.rateDown(book);
+    this.updateSortList(ratedBook);
+  }
+
+  updateSortList(ratedBook: Book) {
+    this.books = this.books
+      .map(b => b.isbn === ratedBook.isbn ? ratedBook : b)
+      .sort((a, b) => b.rating - a.rating);
   }
 }
 
